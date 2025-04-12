6beb9a324a6cd956ebb6b0770ef845bc9ec8c89c
Andreas Sellstone <andreas@sellstone.se>
2025-04-12 16:17:40 +0200

    Removed old JS

--- static/js/old/toc.js
+++
@@ -1,7 +0,0 @@
-export default function initTOC() {
-  document.querySelectorAll(".toc").forEach(toc => {
-    toc.addEventListener("click", () => {
-      toc.classList.toggle("expanded");
-    });
-  });
-}
